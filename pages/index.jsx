144820672d0aff79176a4ae6739620fdff4a043f
AhmetYTK <79027117+AhmetYTK@users.noreply.github.com>
2023-01-24 23:23:58 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,9 +5,7 @@ import Tippy from '@tippyjs/react';
 
 
 export default function Home() {
-  const _repositories  = swr("/api/repos");
-  console.log(_repositories )
-  const repositories = _repositories ? _repositories : null;
+  
 
   return (
     <>
@@ -38,80 +36,7 @@ export default function Home() {
         <p className="text-3xl text-white font-semibold">GitHub Projelerim</p>
 
         <div className="grid grid-cols-1 md:grid-cols-3 w-full gap-4 items-center mt-2">
-          {_repositories
-            ? repositories
-              ? repositories
-                  .sort((a, b) => b.stargazers_count - a.stargazers_count)
-                  .map((_, __) => (
-                    <a
-                      key={__}
-                      href={`https://github.com/ahmetytk/`+_.name}
-                      target="_blank"
-                      rel="noreferrer"
-                      className="bg-[#191932]/20 p-4 hover:bg-[#191932]/30 shadow-lg hover:shadow-xl transition-all duration-200 rounded-lg w-full"
-                    >
-                      <p className="text-md text-white">
-                        <span className="text-sm text-white/50 bg-black/25 px-2 py-1 rounded-md mr-1">
-                          {_.owner.login}
-                        </span>
-                        {_.name}
-                      </p>
-                      <div className="mt-5 flex justify-end w-full h-full items-center">
-                        <div className="flex w-full justify-between items-center">
-                          <Tippy
-                            content={"Stars"}
-                            arrow={false}
-                            animation="shift-away"
-                          >
-                            <div className="flex items-center">
-                              <p className="text-sm">
-                                <i className="fal fa-star mr-2" />
-                              </p>
-                              <p>{_.stargazers_count}</p>
-                            </div>
-                          </Tippy>
-                          <div className="text-sm text-white bg-black/25 px-2 py-1 rounded-md mr-1">
-                            {_.language || "Empty"}
-                          </div>
-                          <Tippy
-                            content={"Forks"}
-                            arrow={false}
-                            animation="shift-away"
-                          >
-                            <div className="flex items-center justify-end">
-                              <p>{_.forks}</p>
-                              <p className="text-sm">
-                                <i className="fal fa-code-branch ml-2" />
-                              </p>
-                            </div>
-                          </Tippy>
-                        </div>
-                      </div>
-                    </a>
-                  ))
-              : Array.from({ length: 6 }).map((_, __) => (
-                  <div
-                    key={__}
-                    className="bg-[#191932]/20 p-4 rounded-lg w-full"
-                  >
-                    <div className="bg-[#191932]/50 animate-pulse w-full h-[28px] rounded-md" />
-                    <div className="mt-5 flex w-full justify-between items-center">
-                      <div className="bg-[#191932]/50 animate-pulse w-12 h-[24px] rounded-md" />
-                      <div className="bg-[#191932]/50 animate-pulse w-24 h-[24px] rounded-md" />
-                      <div className="bg-[#191932]/50 animate-pulse w-12 h-[24px] rounded-md" />
-                    </div>
-                  </div>
-                ))
-            : Array.from({ length: 6 }).map((_, __) => (
-                <div key={__} className="bg-[#191932]/20 p-4 rounded-lg w-full">
-                  <div className="bg-[#191932]/50 animate-pulse w-full h-[28px] rounded-md" />
-                  <div className="mt-5 flex w-full justify-between items-center">
-                    <div className="bg-[#191932]/50 animate-pulse w-12 h-[24px] rounded-md" />
-                    <div className="bg-[#191932]/50 animate-pulse w-24 h-[24px] rounded-md" />
-                    <div className="bg-[#191932]/50 animate-pulse w-12 h-[24px] rounded-md" />
-                  </div>
-                </div>
-              ))}
+<p>Merhaba, ben Ahmet. Lise öğrencisi olarak, YouTube ve Instagram gibi platformlarda içerik üretiyorum. Özellikle Nasıl yapılır, oyun ve araba incelemesi konularını işliyorum. İzleyicilerime eğlenceli ve bilgilendirici içerikler sunmaya çalışıyorum. Ayrıca, yaptığımız Instagram içerikleriyle insanların araba özelliklerine ulaşmasını kolaylaştırmak istiyorum. İnstagram hesabımda güncel Araba gelişmelerini paylaşıyorum ve yaptığım oyun videolarını YouTube kanalımda yayınlıyorum. Siz de benimle birlikte eğlenmek ve bilgi sahibi olmak isterseniz, lütfen YouTube kanalıma abone olup, Instagram hesabımızı takip etmeyi unutmayın!</p>
         </div>
       </div>
 
